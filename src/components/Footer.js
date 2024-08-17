4f87682df9440c516e39d76200b564d3588a782b
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2024-08-17 12:02:41 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,7 +20,7 @@ export const Footer = () => {
                 <a href="https://github.com/varshneyanushka"><img src={navIcon2} alt="" /></a>
                 <a href="https://www.instagram.com/varshney._.anushka/"><img src={navIcon3} alt="" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p> </p>
           </Col>
         </Row>
       </Container>
